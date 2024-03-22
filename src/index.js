7c932e82577f74cc721f46c8b55e4704837724fe
mariatanya <151530541+mariatanya@users.noreply.github.com>
2024-03-22 21:02:01 +0200

    Added the temperature search functionalities using API.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "b4d4bae12atf537f223c2747of06ba2b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInputElement.value;
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Kadoma");
